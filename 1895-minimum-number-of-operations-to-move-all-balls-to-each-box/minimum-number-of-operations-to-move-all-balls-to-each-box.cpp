14d32717447b4850dceeff6b104263525a0e50d1
Furqan Makhdoomi <145181976+furmak331@users.noreply.github.com>
2025-01-06 19:37:02 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (67.01%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,18 +2,39 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n = boxes.size();
-        vector<int> answer(n, 0);  
-        vector<int> validindex;
+        vector<int> answer(n, 0);
+        
+        // Count total 1s and sum of their positions
+        int totalOnes = 0;
+        int positionSum = 0;
         
         for(int i = 0; i < n; i++) {
             if(boxes[i] == '1') {
-                validindex.push_back(i);
+                totalOnes++;
+                positionSum += i;
             }
         }
         
+        int leftOnes = 0;
+        int leftSum = 0;
+        
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < validindex.size(); j++) {
-                answer[i] += abs(validindex[j] - i);
+            // Total moves = moves from left balls + moves from right balls
+            
+            // Left part: number of ones to left * current position - sum of their positions
+            int leftMoves = i * leftOnes - leftSum;
+            
+            // Right part: sum of positions of ones to right - number of ones to right * current position
+            int rightOnes = totalOnes - leftOnes;
+            int rightSum = positionSum - leftSum;
+            int rightMoves = rightSum - (i * rightOnes);
+            
+            answer[i] = leftMoves + rightMoves;
+            
+            // Update left counters if current position has 1
+            if(boxes[i] == '1') {
+                leftOnes++;
+                leftSum += i;
             }
         }
         
