bd6861982d55d5d5a993cffd102c50b47ed60a5e
Furqan Makhdoomi <145181976+furmak331@users.noreply.github.com>
2025-01-11 09:02:50 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (78.67%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
@@ -2,22 +2,16 @@ class Solution {
 public:
     bool canConstruct(string s, int k) {
         if (k > s.length()) return false;
-        
         if (k == s.length()) return true;
         
-        vector<int> freq(26, 0);
+        int oddMask = 0;
+        
         for (char c : s) {
-            freq[c - 'a']++;
+            oddMask ^= (1 << (c - 'a'));
         }
         
-        int oddCount = 0;
-        for (int count : freq) {
-            if (count % 2 != 0) {
-                oddCount++;
-            }
-        }
+        int oddCount = __builtin_popcount(oddMask);
         
-       
         return oddCount <= k;
     }
 };
